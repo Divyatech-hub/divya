efdd1d4970558ac944fb6450980fdf92f422ffec
Divyatech-hub <divyalakshmi5804@gmail.com>
2025-01-03 15:50:37 +0400

    added pages for projects and contact. added about me section to homepage

--- app/layout.jsx
+++ app/layout.jsx
@@ -22,7 +22,7 @@ export default function RootLayout({ children }) {
         <ThemeProvider attribute="class" defaultTheme="light">
           <Header />
           {children}
-          {/* <Footer /> */}
+          <Footer />
         </ThemeProvider>
         </body>
     </html>
